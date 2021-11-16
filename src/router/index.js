28857a1a177f61b41508f8c7a47f85957887efe2
zhiqiang <1215017984@qq.com>
2021-11-16 18:51:10 +0800

    first-commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,21 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-16 17:22:46
+ * @LastEditTime: 2021-11-16 18:01:49
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \后台管理\vue_shop\src\router\index.js
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
